340bdd7e6280d7ddd9821b03213876793c3a2ba6
Chris McCormick <chris@mccormick.cx>
2018-01-04 16:30:18 +0800

    Save state to localStorage.

--- src/soundboard/core.cljs
+++ src/soundboard/core.cljs
@@ -4,41 +4,58 @@
       sfxr
       riffwave))
 
-(defonce sounds (r/atom []))
+(enable-console-print!) 
+
+(defonce sounds (r/atom (try (js->clj (js/JSON.parse (aget js/localStorage "soundboard"))) (catch :default e {}))))
 
 (defn generate-sound [v]
   (try
     ((aget js/sfxr "toAudio") v)
     (catch :default e nil)))
 
-(defn add-empty-slot! [ev]
-  (swap! sounds #(into {(js/Math.random) {:def ""}} %)))
+(defn save! []
+  (js/localStorage.setItem "soundboard" (js/JSON.stringify (clj->js @sounds))))
 
-(defn update-def! [id s ev]
+(defn add-empty-slot! [ev]
+  (swap! sounds #(into {(js/Math.random) {"def" ""}} %))
+  (save!))
+
+(defn update-def! [id ev]
   (let [v (.. ev -target -value)
         sound (generate-sound v)]
-    (swap! sounds assoc id {:def v :sound sound})))
+    (swap! sounds assoc id {"def" v "sound" sound})
+    (save!)))
+
+(defn remove-sound! [id ev]
+  (swap! sounds dissoc id)
+  (save!))
 
 (defn play-sound [synth-def]
-  (.play (synth-def :sound)))
+  (let [snd (get synth-def "sound")]
+    (when snd
+      (.play snd))))
 
 ;; -------------------------
 ;; Views
 
 (defn home-page []
   [:div [:h2 "sndbrd"]
-   (for [[id s] @sounds]
+   (doall (for [[id s] @sounds]
      (if s
        (with-meta
-         [:p [:input.def {:value (s :def)
-                          :on-change (partial update-def! id s)} ]
-          [:button {:on-click #(swap! sounds dissoc id)} "x"]
+         [:p [:input.def {:value (get s "def")
+                          :on-change (partial update-def! id)} ]
+          [:button {:on-click (partial remove-sound! id)} "x"]
           [:button {:on-click (partial play-sound s)} "▶"]]
-         {:key id})))
+         {:key id}))))
    [:p [:button {:on-click add-empty-slot!} "+"]]])
 
 ;; -------------------------
 ;; Initialize app
+
+(doseq [[id s] @sounds]
+  (print id s)
+  (swap! sounds update-in [id] assoc "sound" (generate-sound (get s "def"))))
 
 (defn mount-root []
   (r/render [home-page] (.getElementById js/document "app")))
